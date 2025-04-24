281402d505a4c239625a02a23eabc3089e6fe96b
memory <960298632@qq.com>
2025-04-24 13:48:16 +0800

    优化配置文件

--- 1.12.x/sing-box.js
+++ 1.12.x/sing-box.js
@@ -16,7 +16,7 @@ let proxies = await produceArtifact({
 config.outbounds.push(...proxies)
 
 config.outbounds.map(i => {
-  if (['🚀 节点选择', 'all-♻️ 自动选择'].includes(i.tag)) {
+  if (['🚀 节点选择', '♻️ 自动选择'].includes(i.tag)) {
     i.outbounds.push(...getTags(proxies))
   }
 })
